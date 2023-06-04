a4b366c0844e27f2646c26f1b720703a7462e753
Emmanuel Fosu <111669287+efosu@users.noreply.github.com>
2023-06-04 12:51:55 +0000

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,28 @@
+const toggleButton = document.querySelector('button')!;
+const bodyContainer = document.querySelector('body')!;
+
+
+
+toggleButton.addEventListener('click', () => {
+    bodyContainer.classList.toggle('light');
+    bodyContainer.classList.toggle('dark');
+})
+
+if(matchMedia('(prefers-color-scheme: light').matches) {
+    bodyContainer.classList.add('light');
+    bodyContainer.classList.remove('dark');
+}else {
+    bodyContainer.classList.remove('light');
+    bodyContainer.classList.add('dark');
+}
+
+matchMedia('(prefers-color-scheme: light)').addEventListener('change', (e) => {
+    if(e.matches) {
+        bodyContainer.classList.add('light');
+        bodyContainer.classList.remove('dark');
+    }else {
+        bodyContainer.classList.remove('light');
+        bodyContainer.classList.add('dark');
+    }
+})
+
